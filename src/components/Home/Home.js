f14048f68e25788c8ec0b430e287c1dd64c7c6d1
AMMARJAVED97 <78588149+AMMARJAVED97@users.noreply.github.com>
2021-11-29 22:00:46 +0500

    some
    some changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,74 @@
-import { Button } from "antd"
+import { Form, Input, Button, Checkbox } from 'antd';
 import { Link } from "react-router-dom"
+import './home.css';
+
 
 const Home = () =>{
     return(
-        <>
-        <Button type="primary"><Link to="signin">Home to Signin</Link></Button>
-        <h1>Home</h1>
-        </>
+        <div className="home_bg">
+             <Form className="form_content"
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+     
+    >
+      <Form.Item
+        label="Team Name"
+        name="Team Name"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Team Category"
+        name="Team Category"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Creat Team
+        </Button>
+      </Form.Item>
+    </Form>
+        </div>
     );
 };
+
 export default Home;
